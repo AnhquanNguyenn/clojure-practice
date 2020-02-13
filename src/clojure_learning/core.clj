e7f120664bdaef2a38e3fb6a779a30159912ae3e
Anhquan Nguyen <anhquan.nguyen5417@gmail.com>
2020-02-12 20:38:34 -0600

    add reverse string and loop printing method

--- src/clojure_learning/core.clj
+++ src/clojure_learning/core.clj
@@ -1,7 +1,21 @@
 (ns clojure-learning.core
   (:gen-class))
 
+(def str1 "Hello World!")
+
+(defn reverseString [x]
+  (println "Using just the reverse command:" (reverse x))
+  (println "Using the apply to look nicer:" (apply str (reverse x)))
+)
+
+(defn loopNumberOfTimes [x]
+  (dotimes [_ x]
+    (println str1))    
+)
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println str1)
+  (reverseString str1)
+)
+
